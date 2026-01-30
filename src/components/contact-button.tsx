3e47cec97937ba24ed1d8fb0b5e5abe529a94156
AGomezSittima <agomezsittimadeveloper@gmail.com>
2026-01-30 19:20:00 +0100

    footer contact button aria-label

--- src/components/contact-button.tsx
+++ src/components/contact-button.tsx
@@ -5,13 +5,19 @@ import type { AcceptedLanguage } from "@/i18n";
 
 export function ContactIconButton({
   language,
+  "aria-label": ariaLabel,
 }: {
   language: AcceptedLanguage;
+  "aria-label"?: string;
 }) {
   return (
     <ContactDialog
       trigger={
-        <Button variant="outline" className="cursor-pointer">
+        <Button
+          variant="outline"
+          className="cursor-pointer"
+          aria-label={ariaLabel}
+        >
           <span>
             <LucideMail className="size-5" />
           </span>
