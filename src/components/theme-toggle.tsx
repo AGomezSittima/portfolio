caa5c46770393df905b2711d57c9bfbc9675879c
AGomezSittima <agomezsittimadeveloper@gmail.com>
2026-04-22 19:25:00 +0200

    custom ripple transition for theme toggle

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -1,8 +1,39 @@
 import { LucideMoon, LucideSun } from "lucide-react";
 import { IconButton } from "./icon-button";
+import type { MouseEvent } from "react";
 
 interface ThemeToggleProps {
   tooltip?: string;
+}
+
+function toggleTheme(e: MouseEvent<HTMLButtonElement>, element: HTMLElement) {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  element.style.setProperty("--ripple-x", `${x}px`);
+  element.style.setProperty("--ripple-y", `${y}px`);
+
+  element.classList.add("theme-transition");
+
+  const isDark = element.classList.toggle("dark");
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+}
+
+function handleClick(e: MouseEvent<HTMLButtonElement>) {
+  const element = document.documentElement;
+
+  const astroTransitionRunning = element.hasAttribute("data-astro-transition");
+
+  if (!document.startViewTransition || astroTransitionRunning) {
+    toggleTheme(e, element);
+    return;
+  }
+
+  document
+    .startViewTransition(() => toggleTheme(e, element))
+    .finished.finally(() => {
+      element.classList.remove("theme-transition");
+    });
 }
 
 export function ThemeToggle({ tooltip }: ThemeToggleProps) {
@@ -12,24 +43,7 @@ export function ThemeToggle({ tooltip }: ThemeToggleProps) {
       variant="ghost"
       tooltip={tooltip}
       aria-label={tooltip}
-      onClick={() => {
-        const element = document.documentElement;
-
-        const setTheme = () => {
-          element.classList.toggle("dark");
-          const isDark = element.classList.contains("dark");
-          localStorage.setItem("theme", isDark ? "dark" : "light");
-        };
-
-        if (!document.startViewTransition) {
-          setTheme();
-          return;
-        }
-
-        document.startViewTransition(() => {
-          setTheme();
-        });
-      }}
+      onClick={handleClick}
     >
       <LucideSun className="dark:hidden" />
       <LucideMoon className="hidden dark:block" />
