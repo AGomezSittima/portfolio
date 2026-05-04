fff2b6908eed351b910fc200a7927771505df6d9
AGomezSittima <agomezsittimadeveloper@gmail.com>
2026-05-04 17:31:20 +0200

    markdown with react-markdown

---
+++ src/components/markdown.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+import ReactMarkdown from "react-markdown";
+
+interface Props {
+  content: string;
+  className?: string;
+}
+
+export function Markdown({ content, className }: Props) {
+  return (
+    <div
+      className={cn(
+        "prose md:prose-lg prose-strong:font-bold prose-strong:text-secondary-foreground",
+        className
+      )}
+    >
+      <ReactMarkdown>{content}</ReactMarkdown>
+    </div>
+  );
+}
